dae139c178a21b965e46762daa2049e2c28001f5
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2020-09-17 20:50:09 +0200

    test methods for managing command line parameter rewritten

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -19,6 +19,7 @@ class PseudoTTY(object):
     def isatty(self):
         return self.__isset
 
+
 def test_check_for_redirection():
 
     # sys.stdin = io.StringIO('file1 file2')
@@ -28,25 +29,25 @@ def test_check_for_redirection():
     # test all cases of different redirection combinations
     sys.stdin = PseudoTTY(sys.stdin, True)
     sys.stdout = PseudoTTY(sys.stdout, True)
-    ht.check_for_redirection()
+    ht.GlobalServant.check_for_redirection()
     assert ht.GlobalServant.reading_stdin is False \
            and ht.GlobalServant.redirecting_stdout is False
 
     sys.stdin = io.StringIO('Some file')
     sys.stdout = PseudoTTY(sys.stdout, True)
-    ht.check_for_redirection()
+    ht.GlobalServant.check_for_redirection()
     assert ht.GlobalServant.reading_stdin is True \
            and ht.GlobalServant.redirecting_stdout is False
 
     sys.stdin = PseudoTTY(sys.stdin, True)
     sys.stdout = PseudoTTY(sys.stdout, False)
-    ht.check_for_redirection()
+    ht.GlobalServant.check_for_redirection()
     assert ht.GlobalServant.reading_stdin is False \
            and ht.GlobalServant.redirecting_stdout is True
 
     sys.stdin = io.StringIO('Some file')
     sys.stdout = PseudoTTY(sys.stdout, False)
-    ht.check_for_redirection()
+    ht.GlobalServant.check_for_redirection()
     assert ht.GlobalServant.reading_stdin is True \
            and ht.GlobalServant.redirecting_stdout is True
 
@@ -64,6 +65,7 @@ def test_check_for_redirection():
 
 
 def test_reverse_dns_lookup():
+    ht.GlobalServant.reset()
     ht.gethostbyaddr("172.217.0.0")
     assert ht.GlobalServant.store_dns_lookups["172.217.0.0"]\
            == "ord38s04-in-f0.1e100.net"
@@ -72,7 +74,7 @@ def test_reverse_dns_lookup():
 
 
 def test_manage_params():
-    pass  # Todo
+    ht.GlobalServant.reset()
 
     # test exit params
     args = "--print-event-table".split()
@@ -98,113 +100,122 @@ def test_manage_params():
     assert ht.GlobalServant.std_out == ".output"
     assert ht.GlobalServant.std_err == ".error"
 
-    # args = "--std-log=logging --std-out=output --std-err=error tests/test_logs/valid_logs".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.std_log == ".logging"
-    # assert ht.GlobalServant.std_out == ".output"
-    # assert ht.GlobalServant.std_err == ".error"
-    # # files are set globaly so no more system exit is raised
-    # args = "--std-log logging --std-out output --std-err error".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.std_log == ".logging"
-    # assert ht.GlobalServant.std_out == ".output"
-    # assert ht.GlobalServant.std_err == ".error"
-    #
-    # args = "--show-more std-err,std-out --ignore execution-details," \
-    #        "times,errors,host-nodes,used-resources," \
-    #        "requested-resources,allocated-resources,all-resources".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.show_list == "std-err,std-out".split(',')
-    # assert ht.GlobalServant.ignore_list == "execution-details,times,errors,host-nodes," \
-    #                          "used-resources,requested-resources," \
-    #                          "allocated-resources,all-resources".split(',')
-    # args = "--table-format=plain --table-format simple --table-format github".split()
-    # ht.manage_params(args)
-    # assert ht.table_format == "github"
-    # args = "--table-format=None".split()
-    # ht.manage_params(args)
-    # assert ht.table_format == "github"  # should still be github
-    #
-    # args = "--reverse-dns-lookup --no-config --generate-log -v".split()
-    # ht.manage_params(args)
-    # assert ht.reverse_dns_lookup is True
-    #
-    # args = "--what_is_this".split()
-    # with pytest.raises(SystemExit) as pytest_wrapped_e:
-    #     ht.manage_params(args)
-    # assert pytest_wrapped_e.type == SystemExit
-    # assert pytest_wrapped_e.value.code == 1
-    #
-    # args = "--mode whatever".split()
-    # with pytest.raises(SystemExit) as pytest_wrapped_e:
-    #     ht.manage_params(args)
-    # assert pytest_wrapped_e.type == SystemExit
-    # assert pytest_wrapped_e.value.code == 1
-    #
-    # args = "--mode default".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "default"
-    # args = "--mode d".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "default"
-    # args = "-m default".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "default"
-    # args = "-m d".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "default"
-    #
-    # # all ways to start analyser mode
-    # args = "-a".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "analyse"
-    # args = "--mode analyse".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "analyse"
-    # args = "--mode a".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "analyse"
-    # args = "-m analyse".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "analyse"
-    # args = "-m a".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "analyse"
-    #
-    # # all ways to start summarizer mdoe
-    # args = "-s".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "summarize"
-    # args = "--mode summarize".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "summarize"
-    # args = "--mode s".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "summarize"
-    # args = "-m summarize".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "summarize"
-    # args = "-m s".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "summarize"
-    #
-    # # all ways to start the analysed-summary mode
-    # args = "-as".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "analysed-summary"
-    # args = "--mode analysed-summary".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "analysed-summary"
-    # ht.GlobalServant.mode = None
-    # args = "--mode as".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "analysed-summary"
-    # args = "-m analysed-summary".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "analysed-summary"
-    # args = "-m as".split()
-    # ht.manage_params(args)
-    # assert ht.GlobalServant.mode == "analysed-summary"
+    args = "--show std-err std-out --ignore execution-details " \
+           "times errors host-nodes used-resources " \
+           "requested-resources allocated-resources all-resources".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["show_list"] == "std-err std-out".split()
+    assert res_dict["ignore_list"] == "execution-details times errors " \
+                                      "host-nodes used-resources " \
+                                      "requested-resources " \
+                                      "allocated-resources " \
+                                      "all-resources".split()
+    assert res_dict["table_format"] == "pretty"
+    args = "--table-format=plain".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["table_format"] == "plain"
+
+    args = "--show something".split()
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        ht.manage_params(args)
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 2
+
+    args = "--ignore something".split()
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        ht.manage_params(args)
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 2
+
+    args = "--table something".split()
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        ht.manage_params(args)
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 2
+
+    args = "--reverse-dns-lookup --no-config --generate-log -v".split()
+    res_dict = ht.manage_params(args)
+    assert ht.GlobalServant.reverse_dns_lookup is True
+    assert res_dict["no_config"] is True
+    assert res_dict["generate_log_file"] == "htcompact.log"
+    assert res_dict["verbose"] is True
+
+    args = "--what_is_this".split()
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        ht.manage_params(args)
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 2
+
+    args = "--mode whatever".split()
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        ht.manage_params(args)
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 2
+
+    args = "--mode default".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "default"
+    args = "--mode d".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "default"
+    args = "-m default".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "default"
+    args = "-m d".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "default"
+
+    # all ways to start analyser mode
+    args = "-a".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "analyse"
+    args = "--mode analyse".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "analyse"
+    args = "--mode a".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "analyse"
+    args = "-m analyse".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "analyse"
+    args = "-m a".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "analyse"
+
+    # all ways to start summarizer mdoe
+    args = "-s".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "summarize"
+    args = "--mode summarize".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "summarize"
+    args = "--mode s".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "summarize"
+    args = "-m summarize".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "summarize"
+    args = "-m s".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "summarize"
+
+    # all ways to start the analysed-summary mode
+    args = "-as".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "analysed-summary"
+    args = "--mode analysed-summary".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "analysed-summary"
+    res_dict["mode"] = None
+    args = "--mode as".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "analysed-summary"
+    args = "-m analysed-summary".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "analysed-summary"
+    args = "-m as".split()
+    res_dict = ht.manage_params(args)
+    assert res_dict["mode"] == "analysed-summary"
 
 
 
