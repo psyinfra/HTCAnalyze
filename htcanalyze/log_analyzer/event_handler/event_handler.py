aa69ab8f938435064e5a9f494dc3badbcb019b2a
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-10-07 15:05:06 +0200

    add: get_job_event function

--- htcanalyze/log_analyzer/event_handler/event_handler.py
+++ htcanalyze/log_analyzer/event_handler/event_handler.py
@@ -228,117 +228,39 @@ class EventHandler:
             self.state_manager.state = JobState.ERROR_WHILE_READING
             raise ReadLogException(reason)
 
-
-def get_condor_log(
-        file: str,
-        rdns_lookup=False
-) -> CondorLog:
-    """
-    Read the log file with the htcondor module.
-
-    Return five dicts holding information about:
-    execution node, used resources, times, used ram history and errors
-
-    :type file: str
-    :param file: HTCondor log file
-    :param rdns_lookup: reverse dns lookup for ip-adresses
-    :return: job_details, resources, time_manager, ram_history, errors
-
-    Consider that the return values can be None or empty dictionaries
-    """
-    submission_event = None
-    execution_event = None
-    termination_event = None
-    image_size_events = []
-    occurred_errors = []
-    events = []
-    event_handler = EventHandler()
-    error_while_reading = False
-
-    try:
-        for event in get_events(file):
-            events.append(event)
-    except ReadLogException as err:
-        logging.debug(err)
-        rprint(f"[red]{err}[/red]")
-        error_while_reading = True
-        occurred_errors.append(
-            ErrorEvent(
-                None,
-                None,
-                JobState.ERROR_WHILE_READING,
-                reason=str(err)
-            )
-        )
-
-    for event in events:
-
+    def get_job_event(self, event, rdns_lookup=False) -> JobEvent:
         if event.type == jet.SUBMIT:
-            job_event = event_handler.get_submission_event(event)
-            if isinstance(job_event, JobSubmissionEvent):
-                submission_event = job_event
+            job_event = self.get_submission_event(event)
 
         elif event.type == jet.EXECUTE:
-            job_event = event_handler.get_execution_event(
+            job_event = self.get_execution_event(
                 event,
                 rdns_lookup=rdns_lookup
             )
-            if isinstance(job_event, JobExecutionEvent):
-                execution_event = job_event
 
         elif event.type == jet.IMAGE_SIZE:
-            job_event = event_handler.get_image_size_event(event)
-            image_size_events.append(job_event)
+            job_event = self.get_image_size_event(event)
 
-        # update resource dict and termination date
+            # update resource dict and termination date
         elif event.type == jet.JOB_TERMINATED:
-            job_event = event_handler.get_job_terminated_event(event)
-            termination_event = job_event
+            job_event = self.get_job_terminated_event(event)
 
-        # update error dict and termination date
+            # update error dict and termination date
         elif event.type == jet.JOB_ABORTED:
-            job_event = event_handler.get_job_aborted_event(event)
+            job_event = self.get_job_aborted_event(event)
 
-        # update error dict
+            # update error dict
         elif event.type == jet.JOB_HELD:
-            job_event = event_handler.get_job_held_event(event)
+            job_event = self.get_job_held_event(event)
 
-        # update error dict
+            # update error dict
         elif event.type == jet.SHADOW_EXCEPTION:
-            job_event = event_handler.get_shadow_exception_event(event)
+            job_event = (
+                self.get_shadow_exception_event(event)
+            )
 
         else:
             job_event = None
-            rprint(
-                f"[yellow]Event type: {event.type} "
-                f"not handled yet[/yellow]"
-            )
 
-        if isinstance(job_event, ErrorEvent):
-            occurred_errors.append(job_event)
-            if isinstance(job_event, JobTerminationEvent):
-                termination_event = job_event
+        return job_event
 
-    # End of the file
-
-    if error_while_reading:
-        event_handler.state_manager.state = JobState.ERROR_WHILE_READING
-
-    set_events = SETEvents(
-        submission_event,
-        execution_event,
-        termination_event,
-    )
-    job_details = JobDetails(
-        set_events,
-        event_handler.state_manager
-    )
-    error_events = ErrorEvents(occurred_errors)
-    ram_history = RamHistory(image_size_events)
-
-    return CondorLog(
-        file,
-        job_details,
-        error_events,
-        ram_history
-    )
