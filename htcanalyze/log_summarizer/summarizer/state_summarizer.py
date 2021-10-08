4ab83b07cfd6062af5fd13d305b077f3c8a124ce
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-10-08 13:00:17 +0200

    add: logic for NormalStateSummarizer

--- htcanalyze/log_summarizer/summarizer/state_summarizer.py
+++ htcanalyze/log_summarizer/summarizer/state_summarizer.py
@@ -2,8 +2,24 @@ from abc import abstractmethod
 from typing import List
 
 from htcanalyze.log_analyzer.event_handler import JobState
-from htcanalyze.log_analyzer.condor_log import CondorLog
+from htcanalyze.log_analyzer.condor_log import CondorLog, JobTimes, LogResources
 from .condor_log_summarizer import CondorLogSummarizer
+from .time_summarizer import TimeSummarizer
+from .log_resource_summarizer import LogResourceSummarizer
+from .node_summarizer import NodeSummarizer, SingleNode
+
+
+class SummarizedCondorLogs:
+
+    def __init__(
+            self,
+            avg_times: JobTimes,
+            avg_resources: LogResources,
+            nodes: List[SingleNode]
+    ):
+        self.avg_times = avg_times
+        self.avg_resources = avg_resources
+        self.nodes = nodes
 
 
 class NormalStateSummarizer(CondorLogSummarizer):
@@ -12,10 +28,31 @@ class NormalStateSummarizer(CondorLogSummarizer):
         super(NormalStateSummarizer, self).__init__(condor_logs)
 
     def summarize(self):
-        for condor_log in self.condor_logs:
-            state_manager = condor_log.job_details.state_manager
-            assert state_manager.state == JobState.NORMAL_TERMINATION
-            # Todo
+        resource_summarizer = LogResourceSummarizer(
+            [cl.job_details.resources for cl in self.condor_logs],
+            ignore_empty=False  # Todo
+        )
+        avg_resources = resource_summarizer.summarize()
+        print(avg_resources)
+
+        time_summarizer = TimeSummarizer(
+            [cl.job_details.time_manager for cl in self.condor_logs],
+            ignore_empty=False  # Todo
+        )
+        avg_times = time_summarizer.summarize()
+        print(avg_times)
+
+        node_summarizer = NodeSummarizer(
+            [cl.job_details for cl in self.condor_logs]
+        )
+        nodes = node_summarizer.summarize()
+        print(nodes)
+
+        return SummarizedCondorLogs(
+            avg_times,
+            avg_resources,
+            nodes
+        )
 
 
 class AbnormalStateSummarizer(CondorLogSummarizer):
