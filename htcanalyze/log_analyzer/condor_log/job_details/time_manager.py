1404c1e5a30564a5110d9abfec0653b9e2d86941
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-10-08 11:09:35 +0200

    add: (1) wrapper class for timedelta objects and (2) for Job Times

--- htcanalyze/log_analyzer/condor_log/job_details/time_manager.py
+++ htcanalyze/log_analyzer/condor_log/job_details/time_manager.py
@@ -1,7 +1,74 @@
 """Manage times of HTCondor job logs."""
+
 import json
 from typing import List
 from datetime import datetime as date_time, timedelta
+
+
+class TimeDeltaWrapper(timedelta):
+
+    def __new__(cls, time_delta: timedelta):
+        if time_delta:
+            return timedelta.__new__(
+                cls,
+                days=time_delta.days,
+                seconds=time_delta.seconds,
+                microseconds=time_delta.microseconds
+            )
+        return timedelta.__new__(cls)
+
+    @property
+    def __dict__(self):
+        return str(self)
+
+    def __repr__(self):
+        if self == timedelta():
+            return str(None)
+        return str(self)
+
+
+class JobTimes:
+
+    def __init__(
+            self,
+            waiting_time=None,
+            execution_time=None,
+            total_runtime=None
+    ):
+        self.waiting_time = TimeDeltaWrapper(waiting_time)
+        self.execution_time = TimeDeltaWrapper(execution_time)
+        self.total_runtime = TimeDeltaWrapper(total_runtime)
+
+    def is_empty(self):
+        return (
+            self.waiting_time is None and
+            self.execution_time is None and
+            self.total_runtime is None
+        )
+
+    def __add__(self, other):
+        return JobTimes(
+            self.waiting_time + other.waiting_time,
+            self.execution_time + other.execution_time,
+            self.total_runtime + other.total_runtime
+        )
+
+    def __radd__(self, other):
+        return self if other == 0 else self + other
+
+    def __truediv__(self, other):
+        return JobTimes(
+            self.waiting_time / other,
+            self.execution_time / other,
+            self.total_runtime / other
+        )
+
+    def __repr__(self):
+        return json.dumps(
+            self.__dict__,
+            indent=2,
+            default=lambda x: x.__dict__
+        )
 
 
 class TimeManager:
@@ -22,15 +89,10 @@ class TimeManager:
         self.submission_date = set_events.submission_date
         self.execution_date = set_events.execution_date
         self.termination_date = set_events.termination_date
-        self.waiting_time = None
-        self.execution_time = None
-        self.total_runtime = None
-
         self.running_over_newyear = False
+        self.job_times = self._manage_times()
 
-        self._manage_times()
-
-    def _manage_times(self):
+    def _manage_times(self) -> JobTimes:
         """
         Calculate the waiting, execution and total runtime.
 
@@ -39,6 +101,9 @@ class TimeManager:
         """
         today = date_time.now()
         today = today.replace(microsecond=0)  # remove microseconds
+        waiting_time = None
+        execution_time = None
+        total_runtime = None
 
         # calculate the time difference to last year,
         # if the date is higher that today of running jobs
@@ -47,11 +112,13 @@ class TimeManager:
             if self.submission_date and self.submission_date > today:
                 self.running_over_newyear = True
                 self.submission_date = self.submission_date.replace(
-                    year=self.submission_date.year - 1)
+                    year=self.submission_date.year - 1
+                )
             if self.execution_date and self.execution_date > today:
                 self.running_over_newyear = True
                 self.execution_date = self.execution_date.replace(
-                    year=self.execution_date.year - 1)
+                    year=self.execution_date.year - 1
+                )
 
         if self.execution_date and self.submission_date:
             self.execution_date = self.execution_date
@@ -60,33 +127,40 @@ class TimeManager:
                 self.running_over_newyear = True
                 self.submission_date = self.submission_date.replace(
                     year=self.submission_date.year - 1)
-            self.waiting_time = self.execution_date - self.submission_date
+            waiting_time = self.execution_date - self.submission_date
 
         if self.termination_date:
-            if self.waiting_time:
+            if waiting_time:
                 pass
             if self.execution_date:
                 # new year ?
                 if self.execution_date > self.termination_date:
                     self.running_over_newyear = True
                     self.execution_date = self.execution_date.replace(
-                        year=self.execution_date.year - 1)
-                self.execution_time = (self.termination_date -
-                                       self.execution_date)
+                        year=self.execution_date.year - 1
+                    )
+                execution_time = self.termination_date - self.execution_date
             if self.submission_date:
                 # new year ?
                 if self.submission_date > self.termination_date:
                     self.running_over_newyear = True
                     self.submission_date = self.submission_date.replace(
-                        year=self.submission_date.year - 1)
-                self.total_runtime = (self.termination_date -
-                                      self.submission_date)
+                        year=self.submission_date.year - 1
+                    )
+                total_runtime = self.termination_date - self.submission_date
+
         # Process still running
         elif self.execution_date:
-            self.execution_time = today - self.execution_date
+            execution_time = today - self.execution_date
         # Still waiting for execution
         elif self.submission_date:
-            self.waiting_time = today - self.submission_date
+            waiting_time = today - self.submission_date
+
+        return JobTimes(
+            waiting_time,
+            execution_time,
+            total_runtime
+        )
 
     def create_time_dict(self) -> dict:
         """
@@ -129,9 +203,9 @@ class TimeManager:
         if self.waiting_time:
             time_desc.append("Waiting time")
             time_vals.append(self.waiting_time)
-        if self.execution_time:
+        if self.job_times.execution_time:
             time_desc.append("Execution runtime")
-            time_vals.append(self.execution_time)
+            time_vals.append(self.job_times.execution_time)
         if self.total_runtime:
             time_desc.append("Total runtime")
             time_vals.append(self.total_runtime)
@@ -150,9 +224,9 @@ class TimeManager:
             "submission_date": str(self.submission_date),
             "execution_date": str(self.execution_date),
             "termination_date": str(self.termination_date),
-            "waiting_time": str(self.waiting_time),
-            "execution_time": str(self.execution_time),
-            "total_runtime": str(self.total_runtime),
+            "waiting_time": str(self.job_times.waiting_time),
+            "execution_time": str(self.job_times.execution_time),
+            "total_runtime": str(self.job_times.total_runtime),
             "running_over_newyear": str(self.running_over_newyear)
         }
 
@@ -175,9 +249,9 @@ def calc_avg_on_times(time_managers: List[TimeManager]) -> dict:
     executing_for = timedelta()
     total_running_time = timedelta()
     for manager in time_managers:
-        waiting_for += manager.waiting_time
-        executing_for += manager.execution_time
-        total_running_time += manager.total_runtime
+        waiting_for += manager.job_times.waiting_time
+        executing_for += manager.job_times.execution_time
+        total_running_time += manager.job_times.total_runtime
 
     n_times = len(time_managers)
 
