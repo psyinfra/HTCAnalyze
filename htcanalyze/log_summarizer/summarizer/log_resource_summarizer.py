4c122d7f86cfa6cf2db01775fa34d1c47035c73a
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-10-08 12:48:27 +0200

    add: LogResourceSummarizer

---
+++ htcanalyze/log_summarizer/summarizer/log_resource_summarizer.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from htcanalyze.log_analyzer.condor_log import LogResources
+from .summarizer import Summarizer
+
+
+class LogResourceSummarizer(Summarizer):
+
+    def __init__(self, m_log_resources: List[LogResources] = None, ignore_empty=False):
+        self.m_log_resources = m_log_resources if m_log_resources else []
+        self.ignore_empty = ignore_empty  # todo
+
+    def summarize(self):
+        return sum(self.m_log_resources) / len(self.m_log_resources)
