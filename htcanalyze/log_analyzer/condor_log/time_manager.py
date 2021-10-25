ea802431987e1a650d3c958fbdb8940a68bb2f1e
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-10-25 15:58:37 +0200

    split: _manage_times functionalities

--- htcanalyze/log_analyzer/condor_log/time_manager.py
+++ htcanalyze/log_analyzer/condor_log/time_manager.py
@@ -1,5 +1,6 @@
 """Manage times of HTCondor job logs."""
 import json
+
 from datetime import datetime as date_time, timedelta
 
 from ..event_handler.set_events import SETEvents
@@ -40,9 +41,9 @@ class JobTimes:
 
     def is_empty(self):
         return (
-            self.waiting_time is None and
-            self.execution_time is None and
-            self.total_runtime is None
+                self.waiting_time == timedelta() and
+                self.execution_time == timedelta() and
+                self.total_runtime == timedelta()
         )
 
     def __add__(self, other):
@@ -92,8 +93,56 @@ class TimeManager:
         self.submission_date = submission_date
         self.execution_date = execution_date
         self.termination_date = termination_date
-        self.running_over_newyear = False
+        self.rolled_over_year_boundary = False
+        self._fix_dates_if_rolled_over(
+            self.submission_date,
+            self.execution_date,
+            self.termination_date
+        )
         self.job_times = self._manage_times()
+
+    @classmethod
+    def from_set_events(
+            cls,
+            set_events: SETEvents
+    ):
+        """Overload constructor to init with SETEvents."""
+        return cls(
+            set_events.submission_date,
+            set_events.execution_date,
+            set_events.termination_date
+        )
+
+    def is_empty(self):
+        return (
+                self.submission_date is None and
+                self.execution_date is None and
+                self.termination_date is None
+        )
+
+    def calc_waiting_time(self):
+        if self.submission_date:
+            if self.execution_date:
+                return self.execution_date - self.submission_date
+            elif self.termination_date:
+                return self.termination_date - self.submission_date
+            else:
+                today = date_time.now()
+                today = today.replace(microsecond=0)  # remove microseconds
+                return today - self.submission_date
+
+        return timedelta()
+
+    def calc_execution_time(self):
+        if self.execution_date:
+            if self.termination_date:
+                return self.termination_date - self.execution_date
+            else:
+                today = date_time.now()
+                today = today.replace(microsecond=0)  # remove microseconds
+                return today - self.execution_date
+
+        return timedelta()
 
     def _manage_times(self) -> JobTimes:
         """
@@ -102,68 +151,76 @@ class TimeManager:
         Check also if the times are overlapping in years.
 
         """
-        today = date_time.now()
-        today = today.replace(microsecond=0)  # remove microseconds
-        waiting_time = None
-        execution_time = None
-        total_runtime = None
-
-        # calculate the time difference to last year,
-        # if the date is higher that today of running jobs
-        # this means the execution started before newyear
-        if self.termination_date is None:
-            if self.submission_date and self.submission_date > today:
-                self.running_over_newyear = True
-                self.submission_date = self.submission_date.replace(
-                    year=self.submission_date.year - 1
-                )
-            if self.execution_date and self.execution_date > today:
-                self.running_over_newyear = True
-                self.execution_date = self.execution_date.replace(
-                    year=self.execution_date.year - 1
-                )
-
-        if self.execution_date and self.submission_date:
-            self.execution_date = self.execution_date
-            # new year ?
-            if self.submission_date > self.execution_date:
-                self.running_over_newyear = True
-                self.submission_date = self.submission_date.replace(
-                    year=self.submission_date.year - 1)
-            waiting_time = self.execution_date - self.submission_date
-
-        if self.termination_date:
-            if waiting_time:
-                pass
-            if self.execution_date:
-                # new year ?
-                if self.execution_date > self.termination_date:
-                    self.running_over_newyear = True
-                    self.execution_date = self.execution_date.replace(
-                        year=self.execution_date.year - 1
-                    )
-                execution_time = self.termination_date - self.execution_date
-            if self.submission_date:
-                # new year ?
-                if self.submission_date > self.termination_date:
-                    self.running_over_newyear = True
-                    self.submission_date = self.submission_date.replace(
-                        year=self.submission_date.year - 1
-                    )
-                total_runtime = self.termination_date - self.submission_date
-
-        # Process still running
-        elif self.execution_date:
-            execution_time = today - self.execution_date
-        # Still waiting for execution
-        elif self.submission_date:
-            waiting_time = today - self.submission_date
+        waiting_time = self.calc_waiting_time()
+        execution_time = self.calc_execution_time()
+        total_runtime = waiting_time + execution_time
 
         return JobTimes(
             waiting_time,
             execution_time,
             total_runtime
         )
+
+    @staticmethod
+    def decrease_year(date, val=1):
+        try:
+            return date.replace(
+                year=date.year - val
+            )
+        except ValueError as err:
+            if err.args[0] == 'day is out of range for month':
+                # Likely due to leap year (29.02)
+                # Todo: fix whenever possible with htcondor module
+                return date
+
+    def _fix_dates_if_rolled_over(
+            self,
+            sub_date,
+            exec_date,
+            term_date
+    ):
+        """
+        Decrease the year value of dates if a job rolled-over year boundary.
+        The year value is ambigious and not available in a log file.
+        This should help to calculate time differences correctly.
+
+        Jobs running more than 365 days will NOT be managed correctly.
+
+        :param sub_date:
+        :param exec_date:
+        :param term_date:
+        :return:
+        """
+        # calculate the time difference to last year,
+        # if the date is higher that today of running jobs
+        # this means the execution started before the year overlap
+        rolled_over_year_boundary = False
+        if term_date is None:
+            today = date_time.now()
+            today = today.replace(microsecond=0)  # remove microseconds
+            if sub_date and sub_date > today:
+                rolled_over_year_boundary = True
+                sub_date = self.decrease_year(sub_date)
+            if exec_date and exec_date > today:
+                rolled_over_year_boundary = True
+                exec_date = self.decrease_year(exec_date)
+        else:
+            if exec_date and exec_date > term_date:
+                rolled_over_year_boundary = True
+                exec_date = self.decrease_year(exec_date)
+            if sub_date and sub_date > term_date:
+                rolled_over_year_boundary = True
+                sub_date = self.decrease_year(sub_date)
+
+        if sub_date and exec_date:
+            if sub_date > exec_date:
+                rolled_over_year_boundary = True
+                sub_date = self.decrease_year(sub_date)
+
+        self.submission_date = sub_date
+        self.execution_date = exec_date
+        self.termination_date = term_date
+        self.rolled_over_year_boundary = rolled_over_year_boundary
 
     @property
     def waiting_time(self):
@@ -186,9 +243,8 @@ class TimeManager:
             "waiting_time": str(self.waiting_time),
             "execution_time": str(self.execution_time),
             "total_runtime": str(self.total_runtime),
-            "running_over_newyear": str(self.running_over_newyear)
+            "rolled_over_year_boundary": str(self.rolled_over_year_boundary)
         }
 
     def __repr__(self):
         return json.dumps(self.__dict__)
-
