0cf4b46f1c29f150053f0d3bea539d22f2ea6513
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-10-26 13:31:09 +0200

    use classes for states instead of enums

--- htcanalyze/log_analyzer/event_handler/states.py
+++ htcanalyze/log_analyzer/event_handler/states.py
@@ -1,83 +1,80 @@
-"""State Manager Module."""
+"""State Module."""
+from abc import ABC
 
-from enum import Enum
+
+class State(ABC):
+    pass
 
 
-class State(Enum):
-    NORMAL_TERMINATION = 0
-    ABNORMAL_TERMINATION = 1
-    WAITING = 2
-    RUNNING = 3
-    ERROR_WHILE_READING = 4
-    ABORTED = 5
-    JOB_HELD = 6
-    SHADOW_EXCEPTION = 7
-    UNKNOWN = 8
-    INVALID_HOST_ADDRESS = 9
-    INVALID_USER_ADDRESS = 10
+class JobState(State, ABC):
+    _color: str = None
 
     @property
-    def __dict__(self):
-        return {
-            "name": self.name,
-            "value": self.value
-        }
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, color):
+        self._color = color
 
 
-class ErrorState(Enum):
-    ABORTED = State.ABORTED.value
-    JOB_HELD = State.JOB_HELD.value
-    SHADOW_EXCEPTION = State.SHADOW_EXCEPTION.value
-    ERROR_WHILE_READING = State.ERROR_WHILE_READING.value
-    INVALID_HOST_ADDRESS = State.INVALID_HOST_ADDRESS.value
-    INVALID_USER_ADDRESS = State.INVALID_USER_ADDRESS.value
-
-    @property
-    def __dict__(self):
-        return {
-            "name": self.name,
-            "value": self.value
-        }
+class TerminationState(JobState, ABC):
+    pass
 
 
-class StateColors(Enum):
-    NORMAL_TERMINATION = 'green'
-    BAD_TERMINATION = 'red'
-    RUNNING = 'blue'
-    UNKNOWN_BEHAVIOUR = 'yellow'
-    DEFAULT = 'default'
+class NormalTerminationState(TerminationState):
+
+    def __init__(self):
+        self.color = "green"
 
 
-class JobState(Enum):
-    NORMAL_TERMINATION = State.NORMAL_TERMINATION.value
-    ABNORMAL_TERMINATION = State.ABNORMAL_TERMINATION.value
-    WAITING = State.WAITING.value
-    RUNNING = State.RUNNING.value
-    ERROR_WHILE_READING = State.ERROR_WHILE_READING.value
-    ABORTED = State.ABORTED.value
+class AbnormalTerminationState(TerminationState):
 
-    @property
-    def __dict__(self):
-        return {
-            "name": self.name,
-            "value": self.value
-        }
+    def __init__(self):
+        self.color = "red"
 
-    def get_jobstate_color(self) -> str:
-        if self == self.NORMAL_TERMINATION:
-            return StateColors.NORMAL_TERMINATION.value
-        elif (
-                self == self.ABNORMAL_TERMINATION or
-                self == self.ERROR_WHILE_READING or
-                self == self.ABORTED
-        ):
-            return StateColors.BAD_TERMINATION.value
-        elif (
-                self == self.WAITING or
-                self == self.RUNNING
-        ):
-            return StateColors.RUNNING.value
-        elif self == self.UNKNOWN:
-            return StateColors.UNKNOWN_BEHAVIOUR.value
-        else:
-            return StateColors.DEFAULT.value
+
+class ExecutionState(JobState, ABC):
+
+    def __init__(self):
+        self.color = "blue"
+
+
+class WaitingState(ExecutionState):
+    pass
+
+
+class RunningState(ExecutionState):
+    pass
+
+
+class ErrorState(State, ABC):
+
+    def __init__(self):
+        self.color = "red"
+
+
+class ErrorWhileReadingState(JobState, ErrorState):
+    pass
+
+
+class InvalidHostAddressState(ErrorState):
+    pass
+
+
+class InvalidUserAddressState(ErrorState):
+    pass
+
+
+class AbortedState(TerminationState, ErrorState):
+    pass
+
+
+class JobHeldState(ErrorState):
+    pass
+
+
+class ShadowExceptionState(ErrorState):
+    pass
+
+
