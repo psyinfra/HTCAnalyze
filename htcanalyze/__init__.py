b6090fa5c46f5d8da73865e42eee9fcab503e37f
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-10-28 10:23:09 +0200

    remove: section to create htcanalyze-log files. The verbose mode can be used and simply redirected, so this feature is kind of redundant

--- htcanalyze/__init__.py
+++ htcanalyze/__init__.py
@@ -5,7 +5,8 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-def setup_logging_tool(log_file=None, verbose_mode=False):
+
+def setup_logging_tool(verbose_mode):
     """
         Set up the logging device.
 
@@ -15,37 +16,8 @@ def setup_logging_tool(log_file=None, verbose_mode=False):
         both modes are compatible together
     :return:
     """
-    # disable the loggeing tool by default
+    # disable the logging tool by default
     logging.getLogger().disabled = True
-
-    # I don't know why a root handler is already set,
-    # but we have to remove him in order
-    # to get just the output of our own handler
-    if len(logging.root.handlers) == 1:
-        default_handler = logging.root.handlers[0]
-        logging.root.removeHandler(default_handler)
-
-    # if logging tool is set to use
-    if log_file is not None:
-        # activate logger if not already activated
-        logging.getLogger().disabled = False
-
-        # more specific view into the script itself
-        logging_file_format = '%(asctime)s - [%(funcName)s:%(lineno)d]' \
-                              ' %(levelname)s : %(message)s'
-        file_formatter = logging.Formatter(logging_file_format)
-
-        handler = RotatingFileHandler(
-            log_file,
-            maxBytes=1000000,
-            backupCount=1
-        )
-        handler.setLevel(logging.DEBUG)
-        handler.setFormatter(file_formatter)
-
-        log = logging.getLogger()
-        log.setLevel(logging.DEBUG)
-        log.addHandler(handler)
 
     if verbose_mode:
         # activate logger if not already activated
