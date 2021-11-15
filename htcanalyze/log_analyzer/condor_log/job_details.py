f0da53d2d7529ce0cc1f8c63214d3898e83ca976
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-11-15 15:49:19 +0100

    rebase with pylint: job_details.py

--- htcanalyze/log_analyzer/condor_log/job_details.py
+++ htcanalyze/log_analyzer/condor_log/job_details.py
@@ -1,10 +1,10 @@
 """Save HTCondor job execution details."""
-import json
 
 from htcanalyze import ReprObject
 from ..event_handler.set_events import SETEvents
 from ..event_handler.states import JobState
-from .time_manager import TimeManager
+from .time_manager import TimeManager, JobTimes
+from .logresource import LogResources
 
 
 class JobDetails(ReprObject):
@@ -19,24 +19,27 @@ class JobDetails(ReprObject):
             self,
             set_events: SETEvents,
             state: JobState
-
     ):
         self.set_events = set_events
         self.time_manager = TimeManager.from_set_events(set_events)
         self.state = state
 
     @property
-    def resources(self):
+    def resources(self) -> LogResources:
+        """Returns log resources."""
         return self.set_events.resources
 
     @property
-    def host_address(self):
+    def host_address(self) -> str:
+        """Returns host address."""
         return self.set_events.host_address
 
     @property
-    def submitter_address(self):
+    def submitter_address(self) -> str:
+        """Returns submitter address."""
         return self.set_events.submitter_address
 
     @property
-    def job_times(self):
+    def job_times(self) -> JobTimes:
+        """Returns job times."""
         return self.time_manager.job_times
