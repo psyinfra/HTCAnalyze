2c17cbd4bb7ce18d21d1a5b58e8882ab23edcabf
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-11-15 15:11:56 +0100

    rebase with pylint: set_events.py

--- htcanalyze/log_analyzer/event_handler/set_events.py
+++ htcanalyze/log_analyzer/event_handler/set_events.py
@@ -1,5 +1,8 @@
 """Module for submission, execution and termination events."""
+
+from datetime import datetime
 from htcanalyze import ReprObject
+from .states import TerminationState
 from .job_events import (
     JobSubmissionEvent,
     JobExecutionEvent,
@@ -23,49 +26,56 @@ class SETEvents(ReprObject):
         self.termination_event = termination_event
 
     @property
-    def submission_date(self):
+    def submission_date(self) -> datetime:
+        """Returns submission date"""
         return (
             self.submission_event.time_stamp
             if self.submission_event else None
         )
 
     @property
-    def submitter_address(self):
-        return (
-            self.submission_event.submitter_address
-            if self.submission_event else None
-        )
-
-    @property
-    def execution_date(self):
+    def execution_date(self) -> datetime:
+        """Returns execution date."""
         return (
             self.execution_event.time_stamp
             if self.execution_event else None
         )
 
     @property
-    def host_address(self):
-        return (
-            self.execution_event.host_address
-            if self.execution_event else None
-        )
-
-    @property
     def termination_date(self):
+        """Returns termination date."""
         return (
             self.termination_event.time_stamp
             if self.termination_event else None
         )
 
     @property
-    def termination_state(self):
+    def submitter_address(self) -> str:
+        """Returns submitter address of the job."""
+        return (
+            self.submission_event.submitter_address
+            if self.submission_event else None
+        )
+
+    @property
+    def host_address(self):
+        """Returns host address of execution node."""
+        return (
+            self.execution_event.host_address
+            if self.execution_event else None
+        )
+
+    @property
+    def termination_state(self) -> TerminationState:
+        """Returns the termination state of the job."""
         return (
             self.termination_event.termination_state
             if self.termination_event else None
         )
 
     @property
-    def return_value(self):
+    def return_value(self) -> int:
+        """Returns the return value of the job."""
         return (
             self.termination_event.return_value
             if self.termination_event else None
@@ -73,12 +83,14 @@ class SETEvents(ReprObject):
 
     @property
     def resources(self):
+        """Returns job resources."""
         return (
             self.termination_event.resources
             if self.termination_event else None
         )
 
     def is_empty(self):
+        """Returns True if all events are None."""
         return not (
             self.submission_event or
             self.execution_event or
