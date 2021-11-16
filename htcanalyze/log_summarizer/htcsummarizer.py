9cdbdea86aaef592209a698fdd6ba6e53925209b
Loevenich, Mathis <m.loevenich@fz-juelich.de>
2021-11-16 15:18:25 +0100

    remove: StateSummarizer, add: function to get summarizer by state

--- htcanalyze/log_summarizer/htcsummarizer.py
+++ htcanalyze/log_summarizer/htcsummarizer.py
@@ -1,9 +1,26 @@
 """Module to summarize all condor log files regarding the state."""
 from typing import List
 
-from .state_summarizer import StateSummarizer
-from .summarized_condor_logs.summarized_condor_logs import SummarizedCondorLogs
-from .summarizer.condor_log_summarizer import CondorLogSummarizer
+from htcanalyze.log_analyzer.event_handler.states import (
+    NormalTerminationState,
+    AbnormalTerminationState,
+    WaitingState,
+    RunningState,
+    AbortedState,
+    ErrorWhileReadingState
+)
+from .summarizer.condor_log_summarizer import (
+    CondorLogSummarizer,
+    NormalTerminationStateSummarizer,
+    AbnormalTerminationStateSummarizer,
+    WaitingStateSummarizer,
+    RunningStateSummarizer,
+    AbortedStateSummarizer,
+    ErrorWhileReadingStateSummarizer
+)
+from .summarized_condor_logs.summarized_condor_logs import (
+    SummarizedCondorLogs
+)
 
 
 class HTCSummarizer(CondorLogSummarizer):
@@ -12,7 +29,7 @@ class HTCSummarizer(CondorLogSummarizer):
     def __init__(self, condor_logs):
         super().__init__(condor_logs)
 
-    def initialize_state_dict(self):
+    def _initialize_state_dict(self):
         """Initialize state dictionary."""
         state_dict = {}
         for condor_log in self.condor_logs:
@@ -24,11 +41,31 @@ class HTCSummarizer(CondorLogSummarizer):
 
         return state_dict
 
+    @staticmethod
+    def _get_summarizer_by_state(
+            condor_logs,
+            state
+    ) -> CondorLogSummarizer:
+        if isinstance(state, NormalTerminationState):
+            return NormalTerminationStateSummarizer(condor_logs)
+        if isinstance(state, AbnormalTerminationState):
+            return AbnormalTerminationStateSummarizer(condor_logs)
+        if isinstance(state, WaitingState):
+            return WaitingStateSummarizer(condor_logs)
+        if isinstance(state, RunningState):
+            return RunningStateSummarizer(condor_logs)
+        if isinstance(state, AbortedState):
+            return AbortedStateSummarizer(condor_logs)
+        if isinstance(state, ErrorWhileReadingState):
+            return ErrorWhileReadingStateSummarizer(condor_logs)
+        # else:
+        raise ValueError(f"Unknown state: {state}")
+
     def summarize(self) -> List[SummarizedCondorLogs]:
         """Summarize logs per state."""
-        state_dict = self.initialize_state_dict()
+        state_dict = self._initialize_state_dict()
         state_summarizers = [
-            StateSummarizer(condor_logs, state)
+            self._get_summarizer_by_state(condor_logs, state)
             for state, condor_logs in state_dict.items()
         ]
         return [
